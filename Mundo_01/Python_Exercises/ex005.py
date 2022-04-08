e029f18beaa5a48b261436e5ce0e3da05a01ea38
Helder Henrique <helder.h.silva@grad.ufsc.br>
2022-04-08 00:36:01 -0300

    Added ex005 - Predecessor and Successor

---
+++ Mundo_01/Python_Exercises/ex005.py
@@ -0,0 +1,13 @@
+number = int(input('Digite um número: '))
+
+#
+#ant = number - 1
+#suc = number + 1
+#
+#print(f'O sucessor de {number} é: {suc}')
+#print(f'O antecessor de {number} é: {ant}')
+#
+
+print(f'Número digitado: {number}\n',
+      f'Sucessor = {number + 1}\n',
+      f'Antecessor = {number - 1}')
