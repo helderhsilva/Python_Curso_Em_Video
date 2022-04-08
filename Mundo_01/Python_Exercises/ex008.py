e378e57e7b3b02f68403caaa9bbc1c82d2ddd197
Helder Henrique <helder.h.silva@grad.ufsc.br>
2022-04-08 00:39:47 -0300

    Added ex008 - Measurement Converter

---
+++ Mundo_01/Python_Exercises/ex008.py
@@ -0,0 +1,16 @@
+metro = float(input('Digite a distância em metros: '))
+
+quilometro = metro / 1000
+hectometro = metro / 100
+decametro = metro / 10
+decimetro = metro * 10
+centimetro = metro * 100
+milimetro = metro * 1000
+
+print(f'\nDistância em quilômetros = {quilometro} km')
+print(f'Distância em hectômetro = {hectometro} hm')
+print(f'Distância em decâmetro = {decametro} dam')
+print(f'Distância em metros = {metro} m')
+print(f'Distância em decimetro = {decimetro} dm')
+print(f'Distância em centímetros = {centimetro} cm')
+print(f'Distância em milímetros = {milimetro} mm')
