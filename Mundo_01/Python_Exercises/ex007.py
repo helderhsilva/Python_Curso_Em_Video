a94340e6fbe0934bc1db5ca6515cb056cfaa299f
Helder Henrique <helder.h.silva@grad.ufsc.br>
2022-04-08 00:38:42 -0300

    Added ex007 - Arithmetic Mean

---
+++ Mundo_01/Python_Exercises/ex007.py
@@ -0,0 +1,12 @@
+n1 = float(input('Informe a primeira nota do aluno: '))
+n2 = float(input('Informe a segunda nota do aluno: '))
+
+media = (n1 + n2)/2
+
+print('\nPrimeira nota: {:.1f}'.format(n1))
+print('Segunda nota: {:.1f}'.format(n2))
+print('Média do aluno: {:.1f}'.format(media))
+
+#
+#print('\nMédia do aluno: {:.2f}'.format((n1+n2)/2))
+#
