a0c84ff4fa1ccd0906c2d95df3180682d97f59e1
Helder Henrique <helder.h.silva@grad.ufsc.br>
2022-04-12 00:11:31 -0300

    Added world 1 class 11 exercises

---
+++ Mundo_01/Aula_11/Exemplo.py
@@ -0,0 +1,9 @@
+print('\33[1;31;43mHello World!\033[m')
+
+print('\33[4;33mHello World!\033[m')
+
+print('\33[7;30mHello World!\033[m')
+
+print('\33[0;33;44mHello World!\033[m')
+
+print('\33[7;33;44mHello World!\033[m')
